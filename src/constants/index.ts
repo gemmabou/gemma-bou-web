e3f7134a64e803f82f5bba9eaa6645f39c2380ce
Gemma Bou <gemmabou@outlook.com>
2021-07-28 10:16:18 +0200

    updating CV

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export const CV_URL = "/CV.pdf";
+export const CV_URL = "/Gemma-Bou-CV.pdf";
 export const CV_FILE_NAME = "GemmaBouCV.pdf";
 export const INSTAGRAM_URL = "https://www.instagram.com/gemmabou";
 export const MEDIUM_URL = "https://medium.com/@gemmabou";
